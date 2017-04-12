739fbb2e33a5ef1cfc55bba9ea4296965e08eead
DanielFoks <foks.daniel95@gmail.com>
2017-04-12 16:50:51 +0300

    Asserts was added to test

--- src/test/java/TCPTestIT.java
+++ src/test/java/TCPTestIT.java
@@ -1,8 +1,9 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TCPTestIT {
 
@@ -12,19 +13,21 @@ public class TCPTestIT {
         TCPClient[] tcpClientsBuffer = new TCPClient[10];
 
         int tmp=0;
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 5; i++) {
             if (tmp==10){
                 for (int j = 0; j < 10; j++) {
                     tcpClientsBuffer[j].closeSocket();
                     tcpClientsBuffer[j] = null;
                 }
-                System.out.println("THE BUFFER WAS CLEARED");
                 tmp=0;
             }
-            tcpClients[i]= new TCPClient("Client № "+(i+1),new Socket(InetAddress.getByName("localhost"),33123));
+            tcpClients[i]= new TCPClient("Client № "+(i+1));
+            tcpClients[i].connectToServer(InetAddress.getByName("localhost"),33123);
             tcpClients[i].sendMessage();
+            Assert.assertEquals(tcpClients[i].getMessage() + " -> message(Client № "+(i+1)+")", tcpClients[i].getReceivedMessage());
                 tcpClientsBuffer[tmp] = tcpClients[i];
                 tmp++;
         }
+        Assert.assertEquals(TCPServer.getCountOfClients().get(),new AtomicInteger(5));
     }
 }
